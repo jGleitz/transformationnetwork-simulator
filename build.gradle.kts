9296099fc9a28a45df4f83844f18864208c1e1d8
Joshua Gleitze <dev@joshuagleitze.de>
2019-10-18 11:48:43 +0200

    Minimal Kotlin + React setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.3.50"
+    kotlin("js") version "1.3.50"
     idea
 }
 
@@ -7,14 +7,23 @@ group = "de.joshuagleitze"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    jcenter()
+    maven { setUrl("https://kotlin.bintray.com/kotlin-js-wrappers") }
+}
+
+kotlin {
+    target.browser()
+    sourceSets["main"].dependencies {
+        implementation(npm("react", "16.9.0"))
+        implementation(npm("react-dom", "16.9.0"))
+        implementation(npm("core-js", "3.3.2"))
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib-js"))
+    implementation(name = "kotlin-react-dom", version = "16.9.0-pre.85-kotlin-1.3.50", group = "org.jetbrains")
 }
-
-kotlin.target.browser {}
 
 idea {
     module {
--- src/main/kotlin/ConsoleOutput.kt
+++
@@ -1,5 +0,0 @@
-package de.joshuagleitze.transformationnetwork
-
-fun main(args: Array<String>) {
-    println("Hello JavaScript!")
-}
